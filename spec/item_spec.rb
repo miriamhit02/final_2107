d0a12f98e6577d2e7dfa2a81d9493eba4bca98ec
Miriam Aceves <miriammerari02@gmail.com>
2021-08-11 11:35:25 -0400

    Write tests for new methods

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -6,5 +6,32 @@ RSpec.describe Item do
 
     expect(item1).to be_a(Item)
     expect(item1.name).to eq('Chalkware Piggy Bank')
+    expect(item1.bids).to eq({})
+  end
+
+  it 'can add bid' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+
+    expected = {
+      attendee2 => 20,
+      attendee1 => 22
+    }
+    expect(item1.bids).to eq(expected)
+  end
+
+  it 'can find highest bid' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+
+    expect(item1.current_high_bid).to eq(22)
   end
 end
