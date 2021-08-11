fdc1d34efcd88afec6101a280e3a1d33f4a393e1
Miriam Aceves <miriammerari02@gmail.com>
2021-08-11 11:36:03 -0400

    Build new methods that pass tests

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,24 @@
 class Item
-  attr_reader :name
+  attr_reader :name, :bids
 
   def initialize(name)
     @name = name
+    @bids = {}
+  end
+
+  def add_bid(attendee, amount)
+    if @bids[attendee].nil?
+      @bids[attendee] = amount
+    else
+      @bids[attendee] += amount
+    end
+  end
+
+  def current_high_bid
+    amounts = []
+    @bids.each do |bid, amount|
+      amounts << amount
+    end
+    amounts.max
   end
 end
